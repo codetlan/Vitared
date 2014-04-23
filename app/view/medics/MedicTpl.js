d061c593a97e4793bbb203d2e417bd83f491bcd6
raul <raul@zamacona.me>
2014-04-23 11:33:48 -0500

    numeración

--- app/view/medics/MedicTpl.js
+++ app/view/medics/MedicTpl.js
@@ -12,22 +12,28 @@ Ext.define('Vitared.view.medics.MedicTpl', {
         html = [
             '<tpl if="this.validateDestacado(destacado) == true">' +
                 '<div class="resultados-destacados">' +
-                '<div class="resultado">' +
-                '<img class="fa-map-marker" src="./resources/images/marker-patrocinado.png"/>A' +
-                '<tpl else>',
-            '<div class="resultados">' +
-                '<div class="resultado">' +
-                '<img class="fa-map-marker" src="./resources/images/marker.png"/>1' +
-                '</tpl>' +
+                    '<div class="resultado">' +
+                        '<div class="marker">' +
+                            '<img class="fa-map-marker" src="./resources/images/marker-patrocinado.png"/>' +
+                            '<p class="marker-id">A</p>'+
+                        '</div>'+
+            '<tpl else>',
+                '<div class="resultados">' +
+                    '<div class="resultado">' +
+                        '<div class="marker">' +
+                            '<img class="fa-map-marker" src="./resources/images/marker.png"/>' +
+                            '<p class="marker-id">1</p>'+
+                        '</div>'+
+            '</tpl>' +
                 '<div class="info">' +
-                '<p class="nombre"> Dr. {name} {first_name} {last_name} </p>' +
-                '<p class="especialidad"> {especialidad} </p>' +
-                '<p class="localidad"> {localidad} </p>' +
+                    '<p class="nombre"> Dr. {name} {first_name} {last_name} </p>' +
+                    '<p class="especialidad"> {especialidad} </p>' +
+                    '<p class="localidad"> {localidad} </p>' +
                 '</div><!-- info -->' +
                 '<div class="right">' +
-                '<i class="fa fa-chevron-circle-right" ></i>' +
+                    '<i class="fa fa-chevron-circle-right" ></i>' +
                 '</div><!-- right -->' +
-                '</div><!-- resultado -->',
+            '</div><!-- resultado -->',
             {
                 validateDestacado: function (destacado) {
                     if (destacado != 3) {
